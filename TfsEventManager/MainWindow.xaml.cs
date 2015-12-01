e71902a25d91d9f27172b37da0044cf448bcacc3
Jelle Druyts <jelle_druyts@yahoo.com>
2015-12-01 13:40:17 +0000

    Removed dependency on Team Explorer by using the new application-local client assemblies

--- TfsEventManager/MainWindow.xaml.cs
+++ TfsEventManager/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.TeamFoundation.Framework.Client;
 using System;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -20,17 +18,7 @@ namespace TfsEventManager
             }
             catch (FileNotFoundException exc)
             {
-                if (exc.Message.Contains("Version=10.0.0.0"))
-                {
-                    var entryAssemblyName = Assembly.GetEntryAssembly().GetName();
-                    var message = "This application was compiled against the Team Explorer 2010 assemblies, which you don't seem to have installed." + Environment.NewLine + Environment.NewLine;
-                    message += string.Format(CultureInfo.CurrentCulture, "You can redirect the assembly versions by uncommenting the appropriate lines in the \"{0}.exe.config\" file, depending on the version of Team Explorer you do have installed.", entryAssemblyName.Name);
-                    MessageBox.Show(message, "Team Explorer Version Mismatch", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
-                else
-                {
-                    ShowException(exc);
-                }
+                ShowException(exc);
                 Application.Current.Shutdown();
             }
         }
